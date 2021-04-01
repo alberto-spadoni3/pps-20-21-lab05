6a09149a4e8d303310baf8d036b42494e93aedea
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2021-04-01 12:55:41 +0200

    Mini exam management system implemented

--- src/u05lab/code/Ex2-ExamsManager.scala
+++ src/u05lab/code/Ex2-ExamsManager.scala
@@ -1,7 +1,97 @@
 package u05lab.code
 
-object ExamsManagerTest extends App {
+import u05lab.code.Kind.ExamKind
+import scala.collection.immutable.HashMap
 
-  /* See: https://bitbucket.org/mviroli/oop2018-esami/src/master/a01b/e1/Test.java */
+object Kind {
+    sealed trait ExamKind
+    case object FAILED extends ExamKind
+    case object RETIRED extends ExamKind
+    case object SUCCEEDED extends ExamKind
+}
 
+sealed trait ExamResult {
+    import Kind._
+
+    def getKind(): ExamKind
+    def getEvaluation(): Option[Int]
+    def cumLaude(): Boolean
+}
+
+sealed trait ExamsManager {
+    def createNewCall(call: String): Unit
+    def addStudentResult(call: String, student:String, result: ExamResult): Unit
+    def getAllStudentsFromCall(call: String): Set[String]
+    def getEvaluationsMapFromCall(call: String): HashMap[String, Int]
+}
+
+case class ExamsManagerImpl() extends ExamsManager {
+    private var callsAndStudents: HashMap[String, HashMap[String, ExamResult]] = HashMap.empty
+
+    override def createNewCall(call: String): Unit = {
+        if (callsAndStudents.contains(call))
+            throw new IllegalArgumentException
+
+        callsAndStudents = callsAndStudents + (call -> HashMap.empty)
+    }
+
+    override def addStudentResult(call: String, student: String, result: ExamResult): Unit = {
+        if (!callsAndStudents.contains(call) ||
+            callsAndStudents(call).contains(student))
+            throw new IllegalArgumentException
+
+        callsAndStudents += (call -> (callsAndStudents(call) + (student -> result)))
+    }
+
+    override def getAllStudentsFromCall(call: String): Set[String] = {
+        if (!callsAndStudents.contains(call))
+            throw new IllegalArgumentException
+
+        callsAndStudents(call).keySet
+    }
+
+    override def getEvaluationsMapFromCall(call: String): HashMap[String, Int] = {
+        if (!callsAndStudents.contains(call))
+            throw new IllegalArgumentException
+
+        callsAndStudents(call)
+          .filter(studEval => studEval._2.getEvaluation().isDefined)
+          .map(studEval => (studEval._1 -> studEval._2.getEvaluation().get))
+    }
+}
+
+sealed trait ExamResultFactory {
+    def failed(): ExamResult
+    def retired(): ExamResult
+    def succeededCumLaude(): ExamResult
+    def succeeded(evaluation: Int): ExamResult
+}
+
+case class ExamResultFactoryImpl() extends ExamResultFactory {
+    abstract class AbstractExamResult(private val kind: ExamKind = Kind.FAILED) extends ExamResult {
+        override def getKind(): ExamKind = kind
+        override def getEvaluation(): Option[Int] = Option.empty
+        override def cumLaude(): Boolean = false
+        override def toString: String = kind.toString
+    }
+
+    /**
+      *
+      * @throws IllegalArgumentException - when the evaluation is not in a valid range
+      */
+    abstract class AbstractSucceededExam(private val evaluation: Int) extends AbstractExamResult(Kind.SUCCEEDED) {
+        // vValidating the given evaluation
+        if (evaluation <= 18 && evaluation >= 30)
+            throw new IllegalArgumentException("Evaluation must be in the range 18..30")
+
+        override def getEvaluation(): Option[Int] = Option(evaluation)
+        override def toString: String = super.toString + " with " + evaluation + (if (this.cumLaude()) "L" else "")
+    }
+
+    override def failed(): ExamResult = new AbstractExamResult(){}
+    override def retired(): ExamResult = new AbstractExamResult(Kind.RETIRED){}
+    override def succeeded(evaluation: Int): ExamResult = new AbstractSucceededExam(evaluation){}
+    override def succeededCumLaude(): ExamResult = new AbstractSucceededExam(evaluation = 30) {
+        override def cumLaude(): Boolean = true
+    }
 }
---
+++ test/u05lab/Ex2Tests.scala
@@ -0,0 +1,51 @@
+package u05lab
+
+import org.junit.jupiter.api.Assertions.{assertEquals, assertThrows, assertTrue}
+import org.junit.jupiter.api.Test
+
+import scala.collection.immutable.HashMap
+
+class Ex2Tests {
+    import u05lab.code._
+    @Test
+    def generalTests(): Unit = {
+        val evaluation = 25
+        val erf = ExamResultFactoryImpl()
+        assertEquals(Kind.FAILED.toString, erf.failed().toString)
+        assertEquals(Kind.RETIRED.toString, erf.retired().toString)
+        assertEquals("SUCCEEDED with " + evaluation, erf.succeeded(evaluation).toString)
+        assertEquals("SUCCEEDED with 30" + "L", erf.succeededCumLaude().toString)
+        // assertThrows(classOf[IllegalArgumentException], () => erf.succeeded(3))
+        assertTrue(erf.succeededCumLaude().cumLaude())
+        assertEquals(Option(evaluation), erf.succeeded(evaluation).getEvaluation())
+    }
+
+    @Test
+    def examManagerTests(): Unit = {
+        val em = ExamsManagerImpl()
+        val julyCall = "July"
+        val septemberCall = "September"
+        val student1 = "Alberto"
+        val stud1Evaluation = ExamResultFactoryImpl().succeeded(28)
+        val student2 = "Asia"
+        val stud2Evaluation = ExamResultFactoryImpl().succeededCumLaude()
+
+        em.createNewCall(julyCall)
+        em.createNewCall(septemberCall)
+        em.addStudentResult(julyCall, student1, stud1Evaluation)
+        em.addStudentResult(julyCall, student2, stud2Evaluation)
+
+        assertEquals(Set(student1, student2), em.getAllStudentsFromCall(julyCall))
+        assertEquals(Set.empty, em.getAllStudentsFromCall(septemberCall))
+
+        assertThrows(classOf[IllegalArgumentException], () => em.createNewCall(septemberCall))
+        assertThrows(classOf[IllegalArgumentException], () => em.addStudentResult(julyCall, student2, ExamResultFactoryImpl().failed()))
+        assertThrows(classOf[IllegalArgumentException], () => em.getAllStudentsFromCall("May"))
+
+        val evaluationsMapFromCall = HashMap(student1 -> stud1Evaluation.getEvaluation().get, student2 -> stud2Evaluation.getEvaluation().get)
+        assertEquals(evaluationsMapFromCall, em.getEvaluationsMapFromCall(julyCall))
+        assertEquals(stud2Evaluation.getEvaluation(), em.getEvaluationsMapFromCall(julyCall).get(student2))
+        assertEquals(stud1Evaluation.getEvaluation(), em.getEvaluationsMapFromCall(julyCall).get(student1))
+        assertEquals(Map.empty, em.getEvaluationsMapFromCall(septemberCall))
+    }
+}
